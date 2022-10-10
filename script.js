cee75cdf3a4764943657e19027d34c35e53355b8
Alabi12 <wolo22001@gmail.com>
2022-10-10 23:07:03 +0000

    add mobile manu using javascript syntax

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const menuList = document.querySelector("#menuList");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  menuList.classList.toggle("active");
+});
+
+// document.querySelectorAll(".navLinks").forEach((n) => n.addEventListener("click", () => {
+//     hamburger.classList.remove("active");
+//     menuList.classList.remove("active");
+
+// }));
