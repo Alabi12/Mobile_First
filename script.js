adedd2d903749ecb1d148541238756b0e0502b66
Alabi12 <wolo22001@gmail.com>
2022-10-11 08:53:34 +0000

    add javascript property

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const hamburger = document.querySelector(".hamburger");
 const menuList = document.querySelector("#menuList");
+const logo = document.querySelector(".logo");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   menuList.classList.toggle("active");
+  if (hamburger.hasClass("active")) {
+    logo.classList.toggle("active");
+  }
 });
 
-document.querySelectorAll(".navLinks").forEach((n) => n.addEventListener("click", () => {
+document.querySelectorAll(".navLinks").forEach((n) =>
+  n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     menuList.classList.remove("active");
-
-}));
+    logo.classList.remove("active");
+  })
+);
