263e7b71d346d06443edfbf6051449479ff4b5ac
Alabi12 <wolo22001@gmail.com>
2023-05-10 17:07:04 +0000

    Add project files to the applications

--- script.js
+++ script.js
@@ -49,3 +49,126 @@ window.addEventListener("resize", () => {
   navIcon.firstElementChild.id = "hamburger-icon";
   body.style.overflowY = "scroll";
 });
+
+function downloadResume() {
+  const button = document.querySelector(".btn-class");
+
+  button.click();
+}
+
+function toggleLanguages() {
+  const icon = document.getElementById("language-icon");
+  const list = document.querySelector(".stack_item__dropdown");
+
+  if (list.classList.contains("hidden")) {
+    // Remove the "hidden" class from the list
+    list.classList.remove("hidden");
+
+    // Change the icon to the "down" chevron icon
+    icon.classList.remove("fa-chevron-right");
+    icon.classList.add("fa-chevron-down");
+
+    // Create the HTML elements for the languages and append them to the list
+    const languages = [
+      { name: "REACT", image: "./img/react.png" },
+      { name: "RUBY", image: "./img/ruby.jpeg" },
+      { name: "RAILS", image: "./img/rails.png" },
+      { name: "JAVASCRIPT", image: "./img/javascript.svg" },
+      { name: "HTML", image: "./img/html.svg" },
+    ];
+
+    function createLanguageListItem(language) {
+      const li = document.createElement("li");
+      li.className = "dropdown_item flex";
+      li.innerHTML = `<img src="${language.image}" alt="${language.name} icon" /><span>${language.name}</span>`;
+
+      // Set the image size based on the font size of the text
+      const fontSize = parseInt(
+        getComputedStyle(li.querySelector("span")).fontSize
+      );
+      li.querySelector("img").style.width = `${fontSize}px`;
+      li.querySelector("img").style.height = `${fontSize}px`;
+
+      return li;
+    }
+
+    const languageList = document.querySelector(".stack_item__dropdown");
+
+    for (let i = 0; i < languages.length; i++) {
+      const language = languages[i];
+      const li = createLanguageListItem(language);
+      languageList.appendChild(li);
+    }
+
+    languages.forEach((lang) => {
+      const li = document.createElement("li");
+      li.classList.add("dropdown_item", "flex");
+      li.innerHTML = `
+        <img src="./img/${lang.toLowerCase()}.svg" alt="${lang} icon" />
+        <span>${lang}</span>
+      `;
+      list.appendChild(li);
+    });
+  } else {
+    // Add the "hidden" class to the list
+    list.classList.add("hidden");
+
+    // Change the icon to the "right" chevron icon
+    icon.classList.add("fa-chevron-right");
+    icon.classList.remove("fa-chevron-down");
+
+    // Remove the HTML elements for the languages from the list
+    const languagesList = document.querySelectorAll(
+      ".stack_item__dropdown .dropdown_item"
+    );
+    languagesList.forEach((lang) => list.removeChild(lang));
+  }
+}
+
+const icon = document.getElementById("language-icon");
+icon.addEventListener("click", toggleLanguages);
+
+//display framework and skills
+const toggleIcons = document.querySelectorAll(".toggle-icon");
+
+const contentArr = [
+  [{ name: "REACT" }, { name: "RUBY" }, { name: "RAILS" }],
+
+  [
+    { name: "COMMUNICATION" },
+    { name: "PAIR-PROGRAMMING" },
+    { name: "COLLABORATION " },
+  ],
+];
+
+function showContent(index) {
+  const item = toggleIcons[index].parentNode;
+  const itemContent = item.querySelector(".about_stacks__item__content");
+  itemContent.innerHTML = "";
+
+  contentArr[index].forEach((content) => {
+    const span = document.createElement("span");
+    span.textContent = content.name;
+    itemContent.appendChild(span);
+  });
+
+  itemContent.classList.add("show");
+  toggleIcons[index].classList.remove("fa-chevron-right");
+  toggleIcons[index].classList.add("fa-chevron-down");
+}
+
+toggleIcons.forEach((icon, index) => {
+  icon.addEventListener("click", () => {
+    const itemContent = icon.parentNode.querySelector(
+      ".about_stacks__item__content"
+    );
+
+    if (itemContent.classList.contains("show")) {
+      itemContent.classList.remove("show");
+      icon.classList.remove("fa-chevron-down");
+      icon.classList.add("fa-chevron-right");
+    } else {
+      showContent(index);
+    }
+  });
+});
